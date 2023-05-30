e925c4b8f19082ecc884e7e6a35f1850d8aa8aa0
mouchigua12138 <111183013+mouchigua12138@users.noreply.github.com>
2023-05-30 10:34:21 +0800

    Add files via upload

---
+++ MFCApplication/CDialogCenterPoint.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CDialogCenterPoint 对话框
+
+class CDialogCenterPoint : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDialogCenterPoint)
+
+public:
+	CDialogCenterPoint(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CDialogCenterPoint();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DialogCenterPoint };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	double x;
+	double y;
+	bool   isOK;
+	afx_msg void OnBnClickedOk();
+};
